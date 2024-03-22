f9b23fdaba9ae3e62ade76a9dde62ec6771f2042
RIVERA CARDENAS-EXT <uig18488@contiwan.com>
2024-03-21 23:04:22 -0600

    Hanoi terminado

--- hanoi2.asm
+++ hanoi2.asm
@@ -12,16 +12,12 @@
 	lui a5, 0x10010         # Direcci�n de la torre C
 	addi a5, a5, 64         # Asumiendo que la torre C est� 0x30 bytes despu�s de la torre A
 	
-	addi s3, zero, 0 	# Cantidad de discos en A
-	addi s4, zero, 0 	# Cantidad de discos en B
-	addi s5, zero, 0 	# Cantidad de discos en C
 	add t6, zero, a3
 	
 	for:
 		addi t0, t0, 1
 		sw t0, 0(t6)
 		addi t6, t6, 4
-		addi s3, s3, 4
 		
 		bne t0, a2, for
 	
@@ -35,15 +31,20 @@
 		addi t1, zero, 1
 		bne a2, t1, recursividad
 		
-		add t6, zero, a3
-		add t6, t6, s3
-		
-		lw t0, -4(t6)
-		sw zero, -4(t6)
-		
-		sw t0, 0(a5)
-		addi s5, s5, 4
-		addi s3, s3, -4
+		#OFFSET DE NUESTRA TORRE 1 (a3)
+		sub t2,s9,a2 #s9-a2
+		slli s2,t2,2 #guardamos en s2 el resultado de 4*(s7-a2)
+		add s3,a3,s2 #guardamos en s3 el resultado final de s3 + 4*(s7-a2)
+		    
+		#OFFSET DE NUESTRA TORRE 3 (a5)
+		sub t4,s9,a2  #s9-a2
+		slli s5,t4,2 #guardamos en s2 el resultado de 4*(s7-a2)
+		add s6,a5,s5 #guardamos en s3 el resultado final de s3 + 4*(s7-a2)
+		    
+		#movemos los discos con sus respectivos offsets (de a3 a a5)
+		lw s4,0(s3) #sacamos el disco de la torre 1 con su respectivo offset y lo guardamos en s4
+		sw zero,0(s3) #escribimos 0 en la posicion del disco que recien sacamos para leer
+		sw s4,0(s6) #guardamos el valor de a4 en nuestra torre 3 con su respectivo offset
 		
 		jalr ra
 		
@@ -58,10 +59,10 @@
 			# mod args -> -1
 			addi a2, a2, -1
 			
-			add t4, s4, a4
+			add t3, a4, zero
 			
-			add a4,zero,a5 # a4=a5
-    			add a5,zero,t4 # s5=t4(a5)
+			add a4,a5,zero # a4=a5
+    			add a5,t3,zero # s5=t3(a4)
 			jal hanoi
 			#pop <-
 			lw a5, 20(sp)
@@ -71,15 +72,21 @@
 			lw ra, 4(sp)
 			addi sp, sp, 20
 			
-			add t6, zero, a3
-			add t6, t6, s3
-			
-			lw t0, -4(t6)
-			sw zero, -4(t6)
-			
-			sw t0, 0(a5)
-			addi s4, s4, 4
-			addi s3, s3, -4
+			#OFFSET DE NUESTRA TORRE 1 (a3)
+			sub t2,s9,a2 #s9-a2
+			slli s2,t2,2 #guardamos en s2 el resultado de 4*(s7-a2)
+			add s3,a3,s2 #guardamos en s3 el resultado final de s3 + 4*(s7-a2)
+			    
+			#OFFSET DE NUESTRA TORRE 2(aS)
+			sub t4,s9,a2  #s9-a2
+			slli s5,t4,2 #guardamos en s2 el resultado de 4*(s7-a2)
+			add s6,a5,s5 #guardamos en s3 el resultado final de s3 + 4*(s7-a2)
+			    
+			#movemos los discos con sus respectivos offsets (de a3 a a5)
+			lw s4,0(s3) #sacamos el disco de la torre 1 con su respectivo offset y lo guardamos en s4
+			sw zero,0(s3) #escribimos 0 en la posicion del disco que recien sacamos para leer
+			sw s4,0(s6) #guardamos el valor de a4 en nuestra torre 3 con su respectivo offset
+    
 					
 			#push -> ra, a2
 			addi sp, sp, -20
@@ -91,10 +98,10 @@
 			# mod args -> -1
 			addi a2, a2, -1
 			
-			add t4, s5, a5
+			add t3, a3, zero
 			
-			add a5,zero,t4 # a3=a5
-    			add a3,zero,a4 # s5=t4(a3)
+			add a3,a4,zero # a3=a4
+    			add a4,t3,zero # s5=t3(a3)
     			
 			jal hanoi
 			#pop <-
